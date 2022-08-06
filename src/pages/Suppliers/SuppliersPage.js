8e0ea37cf7be35e9a58733b7692e100d3d38500c
Stuart Laird <salai3@student.monash.edu>
2022-08-06 22:57:21 +1000

    Supplier Data: via HTTP

--- src/pages/Suppliers/SuppliersPage.js
+++ src/pages/Suppliers/SuppliersPage.js
@@ -9,32 +9,9 @@ import {
 import { Container } from "@mui/system";
 import { useReducer, useEffect, useState } from "react";
 import SupplierTable from "../../Components/SupplierTable";
+import useHTTP from "../../hooks/use-http";
 
 const CardTitle = <Typography variant="h5">Suppliers</Typography>;
-
-/*Table Data */
-function createData(
-  supplierId,
-  supplierName
-) {
-  return {
-    supplierId,
-    supplierName
-  };
-}
-
-const rows = [
-  createData("S1234", "Company A"),
-  createData("S5523", "Company B"),
-  createData("S9911", "Company C"),
-  createData("S9233", "Company D"),
-  createData("S1100", "Company E"),
-  createData("S9162", "Company F"),
-  createData("S5542", "Company G"),
-  createData("S0122", "Company H"),
-  createData("S8927", "Company I"),
-  createData("S0128", "Company J"),
-];
 
 const FilterQueryComponent = (props) => {
   return (
@@ -66,7 +43,30 @@ const FilterQueryComponent = (props) => {
 };
 
 const SuppliersPage = () => {
-  const [filteredRows, setFilteredRows] = useState(rows);
+  const [suppliers, setSuppliers] = useState([]);
+  const [filteredRows, setFilteredRows] = useState([]);
+  const { isLoading, error, sendRequest: fetchSuppliers } = useHTTP();
+
+  useEffect(() => {
+    const transformSuppliers = (suppliersObj) => {
+      const loadedSuppliers = [];
+
+      for (const supplierKey in suppliersObj) {
+        loadedSuppliers.push(suppliersObj[supplierKey]);
+      }
+
+      setSuppliers(loadedSuppliers);
+    };
+
+    fetchSuppliers(
+      { url: "https://chace-test-default-rtdb.firebaseio.com/suppliers.json" },
+      transformSuppliers
+    );
+  }, [fetchSuppliers]);
+
+  useEffect(() => {
+    setFilteredRows(suppliers);
+  }, [suppliers, setFilteredRows]);
 
   const initialQueryState = {
     id: "",
@@ -88,12 +88,14 @@ const SuppliersPage = () => {
   );
 
   useEffect(() => {
-    const updatedRows = rows.filter((row) => {
+    const updatedRows = suppliers.filter((row) => {
       return (
         (queryState.id.trim().length === 0 ||
           row.supplierId.includes(queryState.id)) &&
         (queryState.name.trim().length === 0 ||
-          row.supplierName.toLowerCase().includes(queryState.name.toLowerCase()))
+          row.supplierName
+            .toLowerCase()
+            .includes(queryState.name.toLowerCase()))
       );
     });
     setFilteredRows(updatedRows);
@@ -108,7 +110,7 @@ const SuppliersPage = () => {
             queryState={queryState}
             dispatchQuery={dispatchQuery}
           />
-          <SupplierTable rows={filteredRows}/>
+          <SupplierTable rows={filteredRows} />
         </CardContent>
       </Card>
     </Container>
