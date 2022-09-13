a6ef9e7b25f6b7381fe1406a474e762e6c904c24
Stuart Laird <salai3@student.monash.edu>
2022-09-14 09:44:37 +1000

    Modal Wrapper Component

---
+++ src/UI/ModalWrapper.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const ModalWrapper = (props) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>{props.text}</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          {props.children}
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalWrapper;
