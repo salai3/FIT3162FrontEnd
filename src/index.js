f049f0922c264a6e9850a6f6e98bea902ebd39ca
Stuart Laird <salai3@student.monash.edu>
2022-10-15 10:25:53 +1100

    Wrap Browser Router

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import { AuthContextProvider } from "./store/auth-context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthContextProvider>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
-    </AuthContextProvider>
   </React.StrictMode>
 );
 
