ef1cc495bd186bc27e3074908b744075bf1899e6
Stuart Laird <salai3@student.monash.edu>
2022-10-17 21:00:31 +1100

    Authentication: Restrict Pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 
 import '@fontsource/roboto/400.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { useContext } from 'react';
 import Layout from './pages/Layout';
 import SignIn from './pages/Login';
 import LandingPage from './pages/LandingPage';
+import AuthContext from './store/auth-context.js';
 
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<SignIn />} />
-        <Route path="/*" element={<Layout />} />
+        <Route path="*" element={authCtx.isLoggedIn ? <Layout /> : <Navigate to="/login" />} />
         <Route path="/landing" element={<LandingPage />} />
       </Routes>
     </div>
